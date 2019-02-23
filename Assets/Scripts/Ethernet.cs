888d26c0e4c3e31f9644bb7a5887b3e2c92e147e
Piotr Telka <piotr.telka1@gmail.com>
2019-02-24 00:36:32 +0100

    line

---
+++ Assets/Scripts/Ethernet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ethernet : MonoBehaviour
+{
+	private LineRenderer renderer;
+	private Vector3 lastPos;
+	private List<Vector3> positions = new List<Vector3>();
+	public float distance = 1;
+	void Start ()
+	{
+		renderer = GetComponent<LineRenderer>();
+		lastPos = gameObject.transform.position;
+	}
+	
+	void Update () {
+		if (Vector3.Distance(lastPos, gameObject.transform.position) > distance)
+		{
+			SpawnCable();
+			lastPos = gameObject.transform.position;
+		}
+	}
+
+	void SpawnCable()
+	{
+		positions.Add(gameObject.transform.position);
+		renderer.positionCount = positions.Count;
+		renderer.SetPositions(positions.ToArray());
+		Debug.Log(positions.Count);
+	}
+}
