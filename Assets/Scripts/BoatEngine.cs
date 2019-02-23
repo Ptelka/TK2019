ba3fe26cfd6553a62d6c7b83e685b3b0a290b26c
Piotr Telka <piotr.telka1@gmail.com>
2019-02-23 07:00:37 +0100

    Lodka xd

---
+++ Assets/Scripts/BoatEngine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+
+public class BoatEngine : MonoBehaviour {
+	[SerializeField] private int Controller;
+	[SerializeField] private int Position;
+	[SerializeField] private GameObject Oar;
+	
+	
+	private Rigidbody body;
+	private Rigidbody parentBody;
+	private String stick;
+	private float oarRotation;
+	
+	void Start ()
+	{
+		stick = Position == 0 ? InputWrapper.LeftAxisY : InputWrapper.RightAxisY;
+		
+		Debug.Log(stick);
+		
+		body = GetComponent<Rigidbody>();
+		body.isKinematic = true;
+		parentBody = transform.parent.GetComponent<Rigidbody>();
+
+		oarRotation = Oar.transform.localEulerAngles.y;
+	}
+
+
+	private bool stageForward;
+	private bool stageBackward;
+
+	private float prevAxisValue;
+	
+	void Update () {
+		float currectAxisValue = InputWrapper.GetAxis(stick, Controller);
+
+		if (currectAxisValue < prevAxisValue)
+		{
+			parentBody.AddForceAtPosition(new Vector3(0, 0, 0.25f), transform.position);
+		}
+		else if (currectAxisValue > prevAxisValue)
+		{
+			parentBody.AddForceAtPosition(new Vector3(0, 0, -0.09f), transform.position);
+		}
+
+		prevAxisValue = currectAxisValue;
+
+		var ea = Oar.transform.localEulerAngles;
+		ea.y = oarRotation + 20 * currectAxisValue;
+		Oar.transform.localEulerAngles = ea;
+		
+		Debug.Log(currectAxisValue);
+	}
+}
